cf604ce3eafe41178f7a4034cc29c16801e7d2cc
Benedikt Ursprung <bursprung@lbl.gov>
2017-12-08 11:10:44 -0800

    [winspec_remote_readout viewer] made file name consistent to convention, added infline to facilitate reading wavelength values.

--- viewers/winspec_remote_readout_h5.py
+++ viewers/winspec_remote_readout_h5.py
@@ -12,6 +12,9 @@ class WinSpecRemoteReadoutView(DataBrowserView):
         self.ui = self.graph_layout = pg.GraphicsLayoutWidget()
         self.plot = self.graph_layout.addPlot()
         self.plotdata = self.plot.plot()
+        self.infline = pg.InfiniteLine(movable=True, angle=90, label='x={value:0.2f}', 
+           labelOpts={'position':0.8, 'color': (200,200,100), 'fill': (200,200,200,50), 'movable': True})         
+        self.plot.addItem(self.infline)
         
     def on_change_data_filename(self, fname):
         
@@ -21,9 +24,13 @@ class WinSpecRemoteReadoutView(DataBrowserView):
                 self.H = self.dat['measurement/WinSpecRemoteReadout']
             except:
                 self.H = self.dat['measurement/winspec_readout']
-            self.wls = self.H['wls']
+            self.wls = np.array(self.H['wls'])
             self.spectrum = self.H['spectrum']
             self.plotdata.setData(self.wls, np.squeeze(self.spectrum))
+            self.infline.setValue([self.wls.mean(),0 ])
+            
+            self.center_wl = self.dat['hardware/acton_spectrometer/settings'].attrs['center_wl']
+            print('acton center_wl', self.center_wl)
         except Exception as err:
             self.databrowser.ui.statusbar.showMessage("failed to load %s:\n%s" %(fname, err))
             raise(err)
