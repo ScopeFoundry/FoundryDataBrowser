620144705eb9bc098028edea7f317efbe88c02d8
Benedikt Ursprung <bursprung@lbl.gov>
2019-01-07 10:18:25 -0800

    [HyperSpecH5View] now compatible with hyperspec_picam_mcl.h5 files, removed option to norm data (which was moved to HyperSpectralBaseView), options to display different x-axis, solved issues when loading data.

--- viewers/hyperspec_h5.py
+++ viewers/hyperspec_h5.py
@@ -11,25 +11,23 @@ class HyperSpecH5View(HyperSpectralBaseView):
     name = 'hyperspec_h5'
 
     def scan_specific_setup(self):
-        self.spec_plot.setLabel('left', 'Intensity', units='counts')
-        self.spec_plot.setLabel('bottom', 'Wavelength', units='nm')  
+        self.spec_plot.setLabel('left', 'Intensity', units='counts') 
         
         self.integrated_count_map = np.array([[1,2],[3,4]])
         self.median_map = np.array([[1,1],[2,2]])
         
-        self.img_choices = ['integrated_count_map',
-                            'median_map']
+        self.img_choices = ['integrated_count_map']
         self.settings.New('display_image', str, choices=self.img_choices, initial='integrated_count_map')    
-        self.settings.display_image.add_listener(self.on_settings_change)
+        self.settings.display_image.add_listener(self.on_display_image_change)
         
-        self.hyperspec_data_choices = ['spec_map','spec_map_norm']
-        self.settings.New('hyperspec_data', str, choices=self.hyperspec_data_choices, initial='spec_map')    
-        self.settings.hyperspec_data.add_listener(self.on_settings_change)
-                
+        self.x_axis = self.settings.New('x_axis', str, choices=('',))
+        self.x_axis.add_listener(self.on_x_axis_change)
+
         # add settings as a dock
         self.settings_ui = self.settings.New_UI()
-        ds = self.dockarea.addDock(name='settings', position='top', widget=self.settings_ui)
-        ds.setStretch(1,1)
+        ds = self.dockarea.addDock(name='settings', widget=self.settings_ui,
+                                   position='left', relativeTo=self.image_dock)
+        ds.setStretch(1,2)
         
         # add correlation plot as a dock
         self.corr_layout = pg.GraphicsLayoutWidget()
@@ -38,74 +36,91 @@ class HyperSpecH5View(HyperSpectralBaseView):
         self.corr_plot.setLabel('bottom', 'median wls', units='nm') 
         
         self.corr_plotdata = self.corr_plot.plot() 
-        self.dockarea.addDock(name='correlation', position='bottom', widget=self.corr_layout)
+        self.dockarea.addDock(name='correlation', widget=self.corr_layout, 
+                              position='below',  relativeTo = self.spec_dock)
+        self.spec_dock.raiseDock()
+        
+        
+    def is_file_supported(self, fname):
+        return np.any( [(meas_name in fname)
+                            for meas_name in ['m4_hyperspectral_2d_scan', 'andor_hyperspec_scan', 
+                                              'hyperspectral_2d_scan', 'fiber_winspec_scan',
+                                              'hyperspec_picam_mcl.h5','asi_hyperspec_scan']])
+
+    def load_data(self, fname):        
+        
+        self.h5_file = h5py.File(fname)
+        for meas_name in ['m4_hyperspectral_2d_scan', 'hyperspectral_2d_scan', 'andor_hyperspec_scan', 
+                          'fiber_winspec_scan','asi_hyperspec_scan', 'hyperspec_picam_mcl']:
+            if meas_name in self.h5_file['measurement']:
+                self.M = self.h5_file['measurement'][meas_name]
+
+        for map_name in ['hyperspectral_map', 'spec_map']:
+            if map_name in self.M:
+                m = np.array(self.M[map_name])
+                if len(m.shape) == 4:
+                    m = m[0,:,:,:]
+                    
+        self.hyperspec_data = m
+        self.integrated_count_map = self.hyperspec_data.sum(axis=2)
+
+        self.x_axis_choices = []
+        for x_axis_choice in ['wavelength', 'wls', 'wave_numbers', 'raman_shifts']:
+            try:
+                setattr(self, x_axis_choice, np.array(self.M[x_axis_choice]) )
+                self.x_axis_choices.append(x_axis_choice)
+            except Exception as err:
+                #print("failed to find {} array".format(x_axis_choice))
+                pass
+        if len(self.x_axis_choices) == 0:
+            print("failed to find any x_axis choices, generated generic pixels array")
+            self.pixels = np.arange(self.hyperspec_data.shape[-1])
+            self.x_axis_choices.append('pixels')
+            
+        self.x_axis.change_choice_list(self.x_axis_choices)
+        self.x_axis.update_value(self.x_axis_choices[-1])            
+
+
+        #try to add median map
+        if hasattr(self, 'wls'):
+            self.median_map = spectral_median_map(map_=self.hyperspec_data, wls=self.wls)
+            
+        if hasattr(self, 'wavelengths'):
+            self.median_map = spectral_median_map(map_=self.hyperspec_data, wls=self.wavelengths) 
+
+        if hasattr(self, 'median_map'):
+            self.settings.display_image.change_choice_list(self.img_choices + ['median_map'])
+            self.corr_plotdata.setData(x=self.integrated_count_map.flatten(), y=self.median_map.flatten())
         
 
-    def on_settings_change(self):
-        #print('on_settings_change')
+        self.on_display_image_change()
+        self.on_x_axis_change()
+        
+        self.h5_file.close()
+
+
+      
+
+    def on_display_image_change(self):
+        #print('on_display_image_change')
         if hasattr(self, 'display_image'):
             del self.display_image
-            del self.hyperspec_data
 
         self.display_image = getattr(self, self.settings['display_image'])
-        self.hyperspec_data = getattr(self, self.settings['hyperspec_data'])
-        
 #         if self.settings['display_image'] == self.img_choices[1]:
 #             cm = matplotlib_colormap_to_pg_colormap('rainbow')
 #         else:
 #             cm = matplotlib_colormap_to_pg_colormap('viridis')
 #         self.imview.setColorMap(cm)
-        
         self.update_display()
 
-        
-    def is_file_supported(self, fname):
-        return np.any( [(meas_name in fname)
-                            for meas_name in ['m4_hyperspectral_2d_scan', 'andor_hyperspec_scan', 'hyperspectral_2d_scan', 'fiber_winspec_scan','asi_hyperspec_scan']])
 
-    def load_data(self, fname):  
-        if hasattr(self, 'dat'):
-            try:
-                del self.dat
-                del self.M
-                del self.wls
-                del self.spec_map
-                del self.spec_map_norm
-                del self.integrated_count_map
-                del self.median_map
-                print('had dat')
-            except:
-                pass
-        
-        self.dat = h5py.File(fname)
-        for meas_name in ['m4_hyperspectral_2d_scan', 'hyperspectral_2d_scan', 'andor_hyperspec_scan', 'fiber_winspec_scan','asi_hyperspec_scan']:
-            if meas_name in self.dat['measurement']:
-                self.M = self.dat['measurement'][meas_name]
+    def on_x_axis_change(self):        
+        x_axis = self.settings['x_axis']
+        self.spec_x_array = getattr(self, x_axis)
+        self.spec_plot.setLabel('bottom', x_axis)
+        self.update_display()
 
-        if 'hyperspectral_map' in self.M:
-            self.spec_map = np.array(self.M['hyperspectral_map'][0,:,:,:])
-        elif 'spec_map' in self.M:
-            self.spec_map = np.array(self.M['spec_map'][0,:,:,:])
-
-        if len(self.spec_map.shape) > 3:
-            self.spec_map = np.squeeze(self.spec_map)
-
-        try:
-            self.wls = np.array(self.M['wls'])
-        except Exception as err:
-            print("failed to find wls array")
-            self.wls = np.arange(self.spec_map.shape[-1])
-        
-        self.spec_map_norm = norm_map(self.spec_map)
-        self.integrated_count_map = self.spec_map.sum(axis=2)
-        self.median_map = spectral_median_map(map_=self.spec_map, wls=self.wls) 
-              
-        self.spec_x_array = self.wls
-        self.on_settings_change()
-        
-        self.corr_plotdata.setData(x=self.integrated_count_map.flatten(), y=self.median_map.flatten())
-        
-        #print('loaded data', fname)        
 
 def matplotlib_colormap_to_pg_colormap(colormap_name, n_ticks=16):
     '''
