85add51bd891c9299888c7e1ce43a216dac95d1d
Chris Chen <christopherchen@lbl.gov>
2019-04-24 11:35:21 -0700

    [FoundryDataBrowser] Added Hyperspec3DH5View

--- foundry_data_browser.py
+++ foundry_data_browser.py
@@ -103,6 +103,9 @@ app.load_view(HyperSpecCLH5View(app))
 from viewers.hyperspec_h5 import HyperSpecH5View
 app.load_view(HyperSpecH5View(app))
 
+from viewers.hyperspec_3d_h5 import HyperSpec3DH5View
+app.load_view(HyperSpec3DH5View(app))
+
 from viewers.trpl_h5 import TRPLH5View
 app.load_view(TRPLH5View(app))
 
